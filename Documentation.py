cde849ea9f0887550396025d4c38c28762fa25fa
nikolasleeb <vtlaneparking@gmail.com>
2024-09-19 10:29:24 -0400

    Initial commit

---
+++ Documentation.py
@@ -0,0 +1,42 @@
+'''
+
+Documentation
+
+Website code that does allows people to put their yard up
+for rent for tailgating events. Allows people to rent out
+yards for tailgating events.
+
+to use when not attached to github: right-click index and hit run in browse
+
+
+
+app.js: contains code that does...
+
+browse-yards.html:
+
+index.html:
+
+list-yard.html
+
+styles.css:
+
+
+
+Recommendations of implementation:
+predictive address typing
+map embed
+being able to scroll around on map and select lots
+    - when lots are selected, they pop up a bubble to display information
+    - need to create a new lot when a yard gets registered
+attach a database or method of storage
+implement a payment system?
+UI/UX designs
+link to game schedule?
+add pictures of the yard
+add whats allowed and whats not allowed in the yard
+sign on system
+remove examples on browse-yards.html
+error checking, can't book a yard that is already booked, can't book a yard that is your own, etc.
+
+
+'''
---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById('list-yard-btn').onclick = function() {
+    alert('Redirecting to yard listing page...');
+};
+
+document.getElementById('browse-yard-btn').onclick = function() {
+    alert('Redirecting to browse yards page...');
+};
+
